6879b6cde8be1fd8bbf6f72d7b1cbb4c93bad5b0
Krishan <78119683+krrition@users.noreply.github.com>
2026-05-25 18:30:17 -0400

    basic avoidance working

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -1,7 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class NPCController : MonoBehaviour
 {
-    
+    [SerializeField] private NavMeshAgent navAgent;
+    [SerializeField] private Transform target;
+
+
+    private void Start()
+    {
+        navAgent.destination = target.position;
+    }
 }
